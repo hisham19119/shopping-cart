6b238e74d49e2a86bff1f16eb13701864bab3c42
roma0000 <hishamtaha047@gmail.com>
2023-04-23 14:32:32 +0200

    data json & static words

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "../../css/Footer/Footer.css"
+import { words } from '../../words'
+
+
 
 function Footer() {
 return (
     <footer>
-        &copy; copywright Hisham Mohamed 2023
+        {words.footerTitle}
     </footer>
 )
 }
---
+++ client/src/words.js
@@ -0,0 +1,5 @@
+export const words =  {
+    headerTitle : "Shopping Cart" ,
+    footerTitle : "© copywright Hisham Mohamed 2023",
+    content : "content"
+}
